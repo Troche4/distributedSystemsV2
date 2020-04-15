7e16f1c7685fe9f201bb40c723d491fe7985ab7f
rstorey1 <rstorey@luc.edu>
2020-04-15 05:31:30 -0500

    converting list of objects to json strings

--- src/main/java/com/example/distributedsystemsphase2/DB/Converters.java
+++ src/main/java/com/example/distributedsystemsphase2/DB/Converters.java
@@ -1,6 +1,9 @@
 package com.example.distributedsystemsphase2.DB;
 
 import androidx.room.TypeConverter;
+
+import com.google.gson.*;
+
 import java.util.*;
 
 public class Converters {
@@ -17,5 +20,35 @@ public class Converters {
             result = result + element + " ";
         }
         return result;
+    }
+
+    @TypeConverter
+    public static List<String> instructorsToGson(List<Instructor> instructors) {
+        Gson gson = new Gson();
+        List<String> jsonStrings=new ArrayList<>();
+        for(Instructor element: instructors){
+            jsonStrings.add(gson.toJson(element));
+        }
+        return jsonStrings;
+    }
+
+    @TypeConverter
+    public static List<String> quizzesToGson(List<Quiz> quizzes) {
+        Gson gson = new Gson();
+        List<String> jsonStrings=new ArrayList<>();
+        for(Quiz element: quizzes){
+            jsonStrings.add(gson.toJson(element));
+        }
+        return jsonStrings;
+    }
+
+    @TypeConverter
+    public static List<String> multipleChoiceQuestionToGson(List<MultipleChoiceQuestion> multipleChoiceQuestions) {
+        Gson gson = new Gson();
+        List<String> jsonStrings=new ArrayList<>();
+        for(MultipleChoiceQuestion element: multipleChoiceQuestions){
+            jsonStrings.add(gson.toJson(element));
+        }
+        return jsonStrings;
     }
 }
