6bbd1bcd738f38b879fe8e79faba412ad775c199
rstorey1 <rstorey@luc.edu>
2020-04-21 23:57:32 -0500

    added converters

--- src/main/java/com/example/distributedsystemsphase2/DB/Converters.java
+++ src/main/java/com/example/distributedsystemsphase2/DB/Converters.java
@@ -23,7 +23,7 @@ public class Converters {
     }
 
     @TypeConverter
-    public static List<String> instructorsToGson(List<Instructor> instructors) {
+    public static List<String> instructorsToJson(List<Instructor> instructors) {
         Gson gson = new Gson();
         List<String> jsonStrings=new ArrayList<>();
         for(Instructor element: instructors){
@@ -33,7 +33,18 @@ public class Converters {
     }
 
     @TypeConverter
-    public static List<String> quizzesToGson(List<Quiz> quizzes) {
+    public static List<Instructor> jsonToInstructor(List<String> jsonStrings) {
+        List<Instructor> instructorList=new ArrayList<>();
+        for(String element: jsonStrings){
+            Instructor instructor= new Gson().fromJson(element,Instructor.class);
+            instructorList.add(instructor);
+        }
+        return instructorList;
+    }
+
+
+    @TypeConverter
+    public static List<String> quizzesToJson(List<Quiz> quizzes) {
         Gson gson = new Gson();
         List<String> jsonStrings=new ArrayList<>();
         for(Quiz element: quizzes){
@@ -43,12 +54,33 @@ public class Converters {
     }
 
     @TypeConverter
-    public static List<String> multipleChoiceQuestionToGson(List<MultipleChoiceQuestion> multipleChoiceQuestions) {
+    public static List<Quiz> jsonToQuizzes(List<String> jsonStrings) {
+        List<Quiz> quizList=new ArrayList<>();
+        for(String element: jsonStrings){
+            Quiz quiz= new Gson().fromJson(element,Quiz.class);
+            quizList.add(quiz);
+        }
+        return quizList;
+    }
+
+
+    @TypeConverter
+    public static List<String> multipleChoiceQuestionToJson(List<MultipleChoiceQuestion> multipleChoiceQuestions) {
         Gson gson = new Gson();
         List<String> jsonStrings=new ArrayList<>();
         for(MultipleChoiceQuestion element: multipleChoiceQuestions){
             jsonStrings.add(gson.toJson(element));
         }
         return jsonStrings;
+    }
+
+    @TypeConverter
+    public static List<MultipleChoiceQuestion> jsonToMultipleChoiceQuestion(List<String> jsonStrings) {
+        List<MultipleChoiceQuestion> multipleChoiceQuestionList=new ArrayList<>();
+        for(String element: jsonStrings){
+            MultipleChoiceQuestion multipleChoiceQuestion= new Gson().fromJson(element,MultipleChoiceQuestion.class);
+            multipleChoiceQuestionList.add(multipleChoiceQuestion);
+        }
+        return multipleChoiceQuestionList;
     }
 }
